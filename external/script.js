d5226f4e1039e58006541d1d9601e0e208ebaf64
Ashur4maru <nightemere01@gmail.com>
2025-02-11 11:25:58 +0100

    klaar

---
+++ external/script.js
@@ -0,0 +1,6 @@
+'use strict';
+const knoop = document.getElementById('begroeting');
+knoop.addEventListener('click', () => {
+  knoop.textContent = knoop.textContent === 'Hallo!' ? 'Tot ziens!' : 'Hallo!';
+
+});
